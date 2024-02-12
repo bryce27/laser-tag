2225a0be80c73c576521a8599b23d6788773d9a7
Blake-wood <144734481+Blake-wood@users.noreply.github.com>
2024-02-12 17:09:06 -0600

    Add files via upload

---
+++ udpClient.py
@@ -0,0 +1,23 @@
+import socket
+
+clientMsg = "hi"
+start = "202"
+end = "221"
+redBase = "53"
+greenBase = "43"
+serverAddress = 7501
+serverAddressPort = ("127.0.0.1", serverAddress)
+bytesToSend = str.encode(clientMsg)
+bufferSize = 1024
+
+#create client side socket
+udpClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+#send starter message to server from client
+udpClientSocket.sendto(bytesToSend, serverAddress)
+
+#receive message back from server
+serverMsg = udpClientSocket.recvfrom(bufferSize)
+
+#print recieved message
+print("message from the server {}".format(serverMsg[0]))
