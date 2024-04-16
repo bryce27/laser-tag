fa1d691fde931e9b9f5393ae13d1f66c8139b884
darrenb314 <157424559+darrenb314@users.noreply.github.com>
2024-04-16 00:49:22 -0500

    Add files via upload

--- CSCE 3513 Photon/static/game.js
+++ CSCE 3513 Photon/static/game.js
@@ -143,7 +143,7 @@ function generate() {
     let count = 0;
 
     const messageCombinations = [
-        "1:1", "2:1", "1:2", "3:2", "4:3", "3:4", "2:53", "1:43", "2:4"
+        "1:2", "1:3", "2:3", "2:53", "1:43"
     ];
 
     function sendWithDelay() {
